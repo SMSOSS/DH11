7bc626343722b49587220b132f6ad0a1cb6861a3
Wiley Lau <henloboii@protonmail.ch>
2021-11-18 16:48:44 +0800

    DH11: add proper notations to code

--- code.c/code.c.ino
+++ code.c/code.c.ino
@@ -2,24 +2,25 @@
 
 
 
-#define dht_apin   11
+#define dht_apin   11 // define pin node
 dht DHT;  // sensor pin S to pin11
-int temp = 0;
-int hydr = 0;
+int temp = 0; // initialize temperature value to zero
+int hydr = 0; // initialize hydro value to zero
+
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(9600); // label 9600 baud
 }
-
 
 void loop() {
   DHT.read11(dht_apin);
+  // device hasn't been set up yet
   if (temp == 0){
     Serial.println("[INFO] Initializing values");
     temp = DHT.temperature;
 //    hydr = DHT.humidity;
-   }
+   } // finish device setup
 
+  // real bug trigger
   if (DHT.temperature > (temp + 3)){
     Serial.println("[WARN] Potential fire detected");
     Serial.print("Current temperature: ");
@@ -27,13 +28,13 @@ void loop() {
 //    Serial.print("Current humidity: ");
 //    Serial.println(DHT.humidity);
   }
-  else if (DHT.temperature < temp) {
+  else if (DHT.temperature < temp) { // val update
     Serial.println("[INFO] Updating stored values");
     temp = DHT.temperature;
 //    hydr = DHT.humidity;    
   }
   else {
-    Serial.println("[INFO] All stable");
+    Serial.println("[INFO] All stable"); // val update
     temp = DHT.temperature;
 //    hydr = DHT.humidity; 
   }
