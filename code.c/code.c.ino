bb7f949f79461e88e559198c3f9504c3feda7344
Wiley Lau <henloboii@protonmail.ch>
2021-11-18 21:53:41 +0800

    noFlame: switch to flame sensor
    * with flame sensor, we can reduce possible bogus outputs as there is only 0 and 1 for flame

--- code.c/code.c.ino
+++ code.c/code.c.ino
@@ -1,7 +1,7 @@
 #include <dht.h> // load dht lib
 #include <Tone.h> // load tone lib
 
-#define dht_apin   11 // define pin node
+#define flamePin    12 // define flame pin
 #define buzzerPin   8  // define buzzer pin
 #define smokePin    5  // define smoke sensor pin
 #define debugMode   0  // define debug mode
@@ -10,45 +10,30 @@ dht DHT;  // sensor pin S to pin11
 
 int temp = 65536; // initialize temperature value to zero
 int smoke; // smoke val uses integer
+int flame; // flame val uses integer
 
 void setup() {
   Serial.begin(9600); // label 9600 baud
 }
 
 void loop() {
-  DHT.read11(dht_apin);
   smoke = analogRead(smokePin);
-  // device hasn't been set up yet
-  if (temp == 65536){
-    Serial.println("[INFO] Initializing values");
-    temp = DHT.temperature;
-   } // finish device setup
-
+  flame = digitalRead(flamePin);
   // real bug trigger
-  if (DHT.temperature > (temp + 3) && (analogRead(smokePin) > smokeThres){
-    Serial.println("[WARN] Potential fire detected");
-    Serial.print("Current temperature: ");
-    Serial.println(DHT.temperature);
+  if (flame == 1 && (analogRead(smokePin) > smokeThres){
+    Serial.println("[WARN] Potential flame detected");
+    Serial.print("Current flame val: ");
+    Serial.println(flame);
     Serial.print("Current Smoke level :");
     Serial.println(smoke);
     tone(buzzerPin, 2000, 500); // buzz buzz when fire
-  }
-  else if (DHT.temperature < temp) { // val update
-    Serial.println("[INFO] Updating stored values");
-    temp = DHT.temperature;
-    if (debugMode == 1) {
-        Serial.print("[DEBUG] Current temperature value: ");
-        Serial.println(temp);
-        Serial.print("[DEBUG] Current smoke value: ");
-        Serial.println(smoke);
-    }
   }
   else {
     Serial.println("[INFO] All stable"); // val update
     temp = DHT.temperature;
     if (debugMode == 1) {
-        Serial.print("[DEBUG] Current temperature value: ");
-        Serial.println(temp);
+        Serial.print("[DEBUG] Current flame value: ");
+        Serial.println(flame);
         Serial.print("[DEBUG] Current smoke value: ");
         Serial.println(smoke);
     }
