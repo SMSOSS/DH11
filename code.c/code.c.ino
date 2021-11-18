a63ea5096bf2864efac61c90f0f23431920439ca
Wiley Lau <henloboii@protonmail.ch>
2021-11-18 16:57:43 +0800

    DH11: set init value to 65536
    * 65536 is way beyond the sensors' max val. we can use it for init.
    * 0 may also happen in bizzare conditions, making it not a feasible init val.

--- code.c/code.c.ino
+++ code.c/code.c.ino
@@ -4,8 +4,8 @@
 
 #define dht_apin   11 // define pin node
 dht DHT;  // sensor pin S to pin11
-int temp = 0; // initialize temperature value to zero
-int hydr = 0; // initialize hydro value to zero
+int temp = 65536; // initialize temperature value to zero
+int hydr = 65536; // initialize hydro value to zero
 
 void setup() {
   Serial.begin(9600); // label 9600 baud
@@ -14,7 +14,7 @@ void setup() {
 void loop() {
   DHT.read11(dht_apin);
   // device hasn't been set up yet
-  if (temp == 0){
+  if (temp == 65536){
     Serial.println("[INFO] Initializing values");
     temp = DHT.temperature;
 //    hydr = DHT.humidity;
