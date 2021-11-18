975c4a5baaf6e242738a86d22de074483608e532
Wiley Lau <henloboii@protonmail.ch>
2021-11-18 20:21:47 +0800

    DH11: totally drop humidity

--- code.c/code.c.ino
+++ code.c/code.c.ino
@@ -1,11 +1,8 @@
 #include <dht.h> // load lib
 
-
-
 #define dht_apin   11 // define pin node
 dht DHT;  // sensor pin S to pin11
 int temp = 65536; // initialize temperature value to zero
-int hydr = 65536; // initialize hydro value to zero
 
 void setup() {
   Serial.begin(9600); // label 9600 baud
@@ -17,7 +14,6 @@ void loop() {
   if (temp == 65536){
     Serial.println("[INFO] Initializing values");
     temp = DHT.temperature;
-//    hydr = DHT.humidity;
    } // finish device setup
 
   // real bug trigger
@@ -25,18 +21,14 @@ void loop() {
     Serial.println("[WARN] Potential fire detected");
     Serial.print("Current temperature: ");
     Serial.println(DHT.temperature);
-//    Serial.print("Current humidity: ");
-//    Serial.println(DHT.humidity);
   }
   else if (DHT.temperature < temp) { // val update
     Serial.println("[INFO] Updating stored values");
     temp = DHT.temperature;
-//    hydr = DHT.humidity;    
   }
   else {
     Serial.println("[INFO] All stable"); // val update
     temp = DHT.temperature;
-//    hydr = DHT.humidity; 
   }
 
   delay(1000);
