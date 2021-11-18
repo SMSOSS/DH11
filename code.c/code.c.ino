8852223f510f1b1ce81df265b2dd9753090cd6fd
Wiley Lau <henloboii@protonmail.ch>
2021-11-18 16:00:35 +0800

    DH11: reset stats if it doesn't meet fire condition

--- code.c/code.c.ino
+++ code.c/code.c.ino
@@ -20,7 +20,12 @@ void loop() {
     temp = DHT.temperature;
     hydr = DHT.humidity;
    }
-  if (DHT.temperature > temp && hydr > DHT.humidity){
+
+  if (DHT.temperature < temp || hydr > DHT.humidity) {
+    temp = DHT.temperature;
+    hydr = DHT.humidity;    
+  }
+  else if (DHT.temperature > temp && hydr > DHT.humidity){
     Serial.println("[WARN] Potential fire detected");
     Serial.print("Current temperature: ");
     Serial.println(DHT.temperature);
@@ -29,6 +34,8 @@ void loop() {
   }
   else {
     Serial.println("[INFO] All stable");
+    temp = DHT.temperature;
+    hydr = DHT.humidity; 
   }
 
   delay(1000);
