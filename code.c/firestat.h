d26fddeec900c5ad241aea6a6084d918508d5015
Wiley Lau <henloboii@protonmail.ch>
2021-11-24 08:05:14 +0000

    noFire: import dummy firestat header
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

---
+++ code.c/firestat.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 rain2wood@SMSOSS
+ *
+ * File name: firestat.h
+ *
+ * Licensed under WTFPL
+ *
+ * Description: Header used for storing global value that will be used for the project "noFire"
+ */
+
+#ifndef _NOFIRE_FIRESTAT_H
+#define _NOFIRE_FIRESTAT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /*_NOFIRE_FIRESTAT_H*/
